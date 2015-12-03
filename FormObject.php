3529a20c925e6c675cf6fb0ab50db04acc23119a
Franchesco Fonseca <fonseca.franchesco@gmail.com>
2015-12-03 00:24:06 -0430

    new code

--- FormObject.php
+++ FormObject.php
@@ -1,260 +1,273 @@
-
 <?php
 
-abstract Class FormObject(){
-
-
-	/* Autor: Franchesco Fonseca, Webmized.com.
-	 * Este codigo es solo un ejemplo de libre uso, modifiquelo cuanto quiera y uselo para lo que quiera. 
-	 *
-	 * Información IMPORTANTE sobre las [REGLAS]:
-	 * Los metodos que representen reglas de formulario deben ser espeficificados al momentos de añadir la regla
-	 * de la siguiete forma $form->addRule(['attribute' => 'attribute1', 'rule' => 'miregla'])
-	 * y tenga en cuenta que el codigo accede al medoto correspondiente a la regla anteponiendo el prefijo "rule",
-	 * de la siguiente manera dentro del metodo validate $this->ruleMiregla con solo la primera letra del nombre de la
-	 * regla en mayuscula.
-	 * 
-	 * POR LO TANTO, si ud desea agregar una nueva regla siga el mismo patro para el nombre del método que la representa:
-	 * protected function ruleMiregla($attribute){...}  donde a través del parametro attribute llegara el nombre del 
-	 * atributo a validar.
-	 * 
-	 * POR ULTIMO, si el metodo que usted cree para representar una regla, cumple la condicion de error, añada el mensaje 
-	 * de error de la siguiente manera:
-	 *
-	 * $this->$_formErrors[$this->{$attribute}] = $this->attributeLabels($attribute)." "."debe cumplir con X condición";
-	 * 
-	 * donde en esa linea de codigo simplemente estamos cargando el mensaje de error en un array donde deben estar los errores,
-	 * para luego mostrarlos al usuario en el formulario, el valor que se carga no es mas que el label definido para el atributo
-	 * concatenado a un string complementario que permita entender la natturaleza el error.
-	 *
-	 *Información IMPORTANTE sobre los [FILTROS]:
-	 * 
-	 * Los filtros aplican la misma metodología antes descrita para las REGLAS, solo que no se manejan mensajes de error,
-	 * sustituye un valor dado de un atributo por el valor con el filtro ya aplicado.
-	 * 
-	 * Es de notar que los filtros en el método validate() se ejecutan antes que la comprobación de las reglas.
-	 *
-	*/
-
-	/*
-	* las reglas a validar se almacenarán durante la línea de ejecución en este array vacío por defecto de la siguiente manera:
-	* [['attribute' => 'attribute1', 'rule' => 'required'], ['attribute' => 'attribute1', 'rule' => 'integer']]
-	*
-	*/
-	protected $_formRules = [];
-
-	/*
-	* los filtros a aplicar se almacenarán durante la línea de ejecución en este array vacío por defecto de la siguiente manera:
-	* [['attribute' => 'attribute1', 'filter' => 'uppercase'], ['attribute' => 'attribute1', 'filter' => 'trim']]
-	*
-	*/
+abstract Class FormObject
+{
+
+    /*
+     * Este codigo es solo un ejemplo de libre uso, modifiquelo cuanto quiera y uselo para lo que quiera.
+     *
+     * Información IMPORTANTE sobre las [REGLAS]:
+     * Los metodos que representen reglas de formulario deben ser espeficificados al momentos de añadir la regla
+     * de la siguiete forma $form->addRule(['attribute' => 'attribute1', 'rule' => 'miregla'])
+     * y tenga en cuenta que el codigo accede al medoto correspondiente a la regla anteponiendo el prefijo "rule",
+     * de la siguiente manera dentro del metodo validate $this->ruleMiregla con solo la primera letra del nombre de la
+     * regla en mayuscula.
+     *
+     * POR LO TANTO, si ud desea agregar una nueva regla siga el mismo patro para el nombre del método que
+     * la representa: protected function ruleMiregla($attribute){...}  donde a través del parametro attribute
+     * llegara el nombre del  atributo a validar.
+     *
+     * POR ULTIMO, si el metodo que usted cree para representar una regla, cumple la condicion de error, añada el
+     * mensaje de error de la siguiente manera:
+     *
+     * $this->$_formErrors[$this->{$attribute}] = $this->attributeLabels($attribute)." "."debe cumplir con X condición";
+     *
+     * donde en esa linea de codigo simplemente estamos cargando el mensaje de error en un array donde deben estar los
+     * errores, para luego mostrarlos al usuario en el formulario, el valor que se carga no es mas que el label definido
+     * para el atributo concatenado a un string complementario que permita entender la natturaleza el error.
+     *
+     * Información IMPORTANTE sobre los [FILTROS]:
+     *
+     * Los filtros aplican la misma metodología antes descrita para las REGLAS, solo que no se manejan mensajes de
+     * error, sustituye un valor dado de un atributo por el valor con el filtro ya aplicado.
+     *
+     * Es de notar que los filtros en el método validate() se ejecutan antes que la comprobación de las reglas.
+     *
+    */
+
+    /*
+    * las reglas a validar se almacenarán durante la línea de ejecución en este array vacío por defecto de la siguiente
+    * manera:
+    * [['attribute' => 'attribute1', 'rule' => 'required'], ['attribute' => 'attribute1', 'rule' => 'integer']]
+    *
+    */
+    protected $_formRules = [];
+
+    /*
+    * los filtros a aplicar se almacenarán durante la línea de ejecución en este array vacío por defecto de la
+    * siguiente manera:
+    * [['attribute' => 'attribute1', 'filter' => 'uppercase'], ['attribute' => 'attribute1', 'filter' => 'trim']]
+    *
+    */
+
+    protected $_formFilters = [];
+
+
+    /*
+    * los errores encontrados al validar las reglas se almacenarán en este array vacío de la siguiete manera para luego
+    *  ser mostrados al usuario:
+    *
+    * ['attribute1' => ['mensaje del error 1', 'mensaje del error N...'],
+    * 'attribute34' => ['mensaje del error 1', 'mensaje del error N...'],]
+    *
+    */
+
+    protected $_formErrors = [];
+
+
+    //metodo para registrar errores presentes en un campo del formulario
+    protected function addError($attribute, $msg)
+    {
+        if (!isset($this->_formErrors[$this->{$attribute}])) {
+
+            $this->_formErrors[$this->{$attribute}] = [];
+        }
 
-	protected $_formFilters = [];
+        $this->_formErrors[$this->{$attribute}][] = $msg;
+    }
 
+    public function attributeLabel($attribute)
+    {
 
-	/*
-	* los errores encontrados al validar las reglas se almacenarán en este array vacío de la siguiete manera para luego ser mostrados al usuario:
-	* ['attribute1' => ['mensaje del error 1', 'mensaje del error N...'], 'attribute34' => ['mensaje del error 1', 'mensaje del error N...'],]
-	*
-	*/
+        // attribute => label
+        $attributeLabels = [
 
-	protected $_formErrors = [];
+        ];
 
 
-	//metodo para registrar errores presentes en un campo del formulario
-	protected function addError($attribute, $msg)
-	{
-		if(!isset($this->$_formErrors[$this->{$attribute}])){
+        return ((isset($attributeLabels[$attribute]) ? $attributeLabels[$attribute] : null));
+    }
 
-			$this->$_formErrors[$this->{$attribute}] = [];
-		}
 
-		$this->$_formErrors[$this->{$attribute}][] = $msg;
-	}
+    //devuelve un error si el tipo de dato no es entero
+    protected function ruleInteger($attribute)
+    {
 
-	public function attributeLabel($attribute)
-	{
+        if (!is_int($this->{$attribute})) {
 
-		// attribute => label
-		$attributeLabels = [
+            $this->addError($attribute, $this->attributeLabels($attribute) . " " . "debe ser un número entero");
+        }
+    }
 
-		];
+    //devuelve un error si el tipo de dato no es double
+    protected function ruleDouble($value)
+    {
 
+        if (!is_double($this->{$attribute})) {
 
-		return ((isset($attributeLabels[$attribute])?  $attributeLabels[$attribute] : null));
-	}
+            $this->addError($attribute,
+                $this->attributeLabels($attribute) . " " . "debe ser un número de doble precisión");
+        }
 
+    }
 
-	//devuelve un error si el tipo de dato no es entero
-	protected function ruleInteger($attribute)
-	{
+    //devuelve un error de datos faltantes si la validación no se cumple
+    protected function ruleRequired($value)
+    {
 
-		if(!is_int($this->{$attribute})){
+        if (empty($this->{$attribute})) {
 
-			$this->addError($attribute, $this->attributeLabels($attribute)." "."debe ser un número entero");
-		}
-	}
+            $this->addError($attribute, $this->attributeLabels($attribute) . " " . "es requerido");
+        }
+    }
 
-	//devuelve un error si el tipo de dato no es double
-	protected function ruleDouble($value)
-	{
+    //asegura que lo que contenga el atributo no tenga espacio en blanco a los extremos
+    protected function filterTrim($attribute)
+    {
+        $this->{$attribute} = strtoupper($this->{$attribute});
+    }
 
-		if(!is_double($this->{$attribute})){
+    //asegura que lo que contenda el atributo se encuentre en mayuscula
+    protected function filterUppercase($attribute)
+    {
+        $this->{$attribute} = strtoupper($this->{$attribute});
+    }
 
-			$this->addError($attribute, $this->attributeLabels($attribute)." "."debe ser un número de doble precisión");
-		}
+    //asegura que lo que contenga el atributo se encuentre en minuscula
+    protected function filterLowercase($attribute)
+    {
+        $this->{$attribute} = strtolower($this->{$attribute});
+    }
 
-	}
 
-	//devuelve un error de datos faltantes si la validación no se cumple
-	protected function ruleRequired($value)
-	{
+    public function addRule($rule)
+    {
 
-		if(empty($this->{$attribute})){
+        if (!is_array($rule)) {
 
-			$this->addError($attribute, $this->attributeLabels($attribute)." "."es requerido");
-		}
-	}
+            throw new Exception("Malformación de regla de formulario, una regla debe ser un array con la siguiente
+             forma ['attribute' => attributeName, 'rule' => ruleName]");
+        }
 
-	//asegura que lo que contenga el atributo no tenga espacio en blanco a los extremos
-	protected function filterTrim($attribute)
-	{
-		$this->{$attribute} = strtoupper($this->{$attribute});
-	}
+        if (!array_key_exists('attribute', $rule) || !array_key_exists('rule', $rule)) {
 
-	//asegura que lo que contenda el atributo se encuentre en mayuscula
-	protected function filterUppercase($attribute)
-	{
-		$this->{$attribute} = strtoupper($this->{$attribute});
-	}
+            throw new Exception("Malformación de regla de formulario, una regla representada en un array debe
+             tener obligatoriamente la llave attribute y la llave rule");
+        }
 
-	//asegura que lo que contenga el atributo se encuentre en minuscula
-	protected function filterLowercase($attribute)
-	{
-		$this->{$attribute} = strtolower($this->{$attribute});
-	}
+        if (count($rule) < 2 || count($rule) > 2) {
 
+            throw new Exception("Malformación de regla de formulario, una regla puede tener solo
+             2 posiciones en el array");
+        }
 
-	public function addRule($rule)
-	{
+        return $this->_formRules[] = $rule;
+    }
 
-		if(!is_array($rule)){
 
-			throw new Exception("Malformación de regla de formulario, una regla debe ser un array con la siguiente forma ['attribute' => attributeName, 'rule' => ruleName]");	
-		}
+    public function addFilter($filter)
+    {
 
-		if(!array_key_exists('attribute', $rule) || !array_key_exists('rule', $rule)){
+        if (!is_array($filter)) {
 
-			throw new Exception("Malformación de regla de formulario, una regla representada en un array debe tener obligatoriamente la llave attribute y la llave rule");
-		}
+            throw new Exception("Malformación de filtro de formulario, un filtro debe ser un array con la siguiente
+             forma ['attribute' => attributeName, 'filter' => filterName]");
+        }
 
-		if(count($rule) < 2 || count($rule) > 2){
-			
-			throw new Exception("Malformación de regla de formulario, una regla puede tener solo 2 posiciones en el array");
-		}
+        if (!array_key_exists('attribute', $filter) || !array_key_exists('rule', $filter)) {
 
-		return $this->$_formRules[] = $rule;
-	}
+            throw new Exception("Malformación de filtro de formulario, un filtro representado en un array debe tener
+             obligatoriamente la llave attribute y la llave filter");
+        }
 
+        if (count($filter) < 2 || count($filter) > 2) {
 
-	public function addFilter($filter)
-	{
+            throw new Exception("Malformación de filtro de formulario, un filtro puede tener solo 2
+             posiciones en el array");
+        }
 
-		if(!is_array($filter)){
+        return $this->_defaultFilters[] = $filter;
+    }
 
-			throw new Exception("Malformación de filtro de formulario, un filtro debe ser un array con la siguiente forma ['attribute' => attributeName, 'filter' => filterName]");	
-		}
 
-		if(!array_key_exists('attribute', $filter) || !array_key_exists('rule', $filter)){
+    public function load($array_attributes)
+    {
 
-			throw new Exception("Malformación de filtro de formulario, un filtro representado en un array debe tener obligatoriamente la llave attribute y la llave filter");
-		}
+        if (isset($array_attributes[get_class($this)])) {
 
-		if(count($filter) < 2 || count($filter) > 2){
-			
-			throw new Exception("Malformación de filtro de formulario, un filtro puede tener solo 2 posiciones en el array");
-		}
+            if (is_array($array_attributes[get_class($this)])) {
 
-		return $this->$_defaultFilters[] = $filter;
-	}
+                foreach ($array_attributes[get_class($this)] as $attribute => $value) {
 
+                    if (isset($this->{$attribute})) {
 
-	public function load($array_attributes)
-	{
+                        $this->{$attribute} = $value;
+                    } else {
 
-		if(isset($array_attributes[get_class($this)])){
+                        throw new Exception("el atributo {$attribute} no existe");
+                    }
+                }
 
-			if(is_array($array_attributes[get_class($this)])){
+                return true;
+            }
 
-				foreach($array_attributes[get_class($this)] as $attribute => $value){
+        } else {
 
-					if(isset($this->{$attribute})){
+            throw new Exception("No fue recibido ningún dato");
+        }
 
-						$this->{$attribute} = $value;
-					}else{
-						
-						throw new Exception("el atributo {$attribute} no existe");
-					}
-				}
+        return false;
 
-				return true;
-			}
+    }
 
-		}else{
+    public function validate()
+    {
 
-			throw new Exception("No fue recibido ningún dato");
-		}
+        //aplicar todos los filtros
+        foreach ($this->_formFilters as $filterToApply) {
 
-		return false;
-		
-	}
+            $attributeToEval = $filterToApply['attribute'];
 
-	public function validate()
-	{
+            if (!isset($this->{$attributeToEval})) {
 
-		//aplicar todos los filtros
-		foreach($this->$_formfilters as $filterToApply){
+                throw new Exception("El atributo {$attributeToEval} no existe");
+            }
 
-			$attributeToEval = $filterToApply['attribute'];
+            $methoTobeUsed = "filter" . ucfirst($filterToApply['attribute']);
 
-			if(!isset($this->{$attributeToEval}){
+            if (!method_exists($this, $methoTobeUsed)) {
 
-				throw new Exception("El atributo {$attributeToEval} no existe");
-			}
+                throw new Exception("El método {$methoTobeUsed} no existe, no se puede filtrar
+                 el atributo {$attributeToEval}");
+            }
 
-			$methoTobeUsed = "filter".ucfirst($filterToApply['attribute']);
+            $this->{$methoTobeUsed}($attributeToEval);
 
-			if(!method_exists($this, $methoTobeUsed)){
+        }
 
-				throw new Exception("El método {$methoTobeUsed} no existe, no se puede filtrar el atributo {$attributeToEval}");
-			}
+        //validar todas las reglas
+        foreach ($this->_formRules as $ruleToEval) {
 
-			$this->{$methoTobeUsed}($attributeToEval);
-			
-		}
+            $attributeToEval = $ruleToEval['attribute'];
 
-		//validar todas las reglas
-		foreach($this->$_formRules as $ruleToEval){
+            if (!isset($this->{$attributeToEval})) {
 
-			$attributeToEval = $ruleToEval['attribute'];
+                throw new Exception("El atributo {$attributeToEval} no existe");
+            }
 
-			if(!isset($this->{$attributeToEval}){
+            $methoTobeUsed = "rule" . ucfirst($ruleToEval['attribute']);
 
-				throw new Exception("El atributo {$attributeToEval} no existe");
-			}
+            if (!method_exists($this, $methoTobeUsed)) {
 
-			$methoTobeUsed = "rule".ucfirst($ruleToEval['attribute']);
+                throw new Exception("El método {$methoTobeUsed} no existe, no se puede validar
+                 el atributo {$attributeToEval}");
+            }
 
-			if(!method_exists($this, $methoTobeUsed)){
+            $this->{$methoTobeUsed}($attributeToEval);
 
-				throw new Exception("El método {$methoTobeUsed} no existe, no se puede validar el atributo {$attributeToEval}");
-			}
+        }
 
-			$this->{$methoTobeUsed}($attributeToEval);
-			
-		}
-
-	}
+    }
 
 }
 
